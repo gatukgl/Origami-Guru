279befca06f2c2fb116850cf8665c53a5cde3ae8
gatukgl <gatukgl@gatukgls-MacBook-Pro.local>
2015-03-02 13:59:21 +0700

    This is new Origami Guru unity project

---
+++ Assets/Scripts/TrackableList.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TrackableList : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		// Get the StateManager
+		StateManager sm = TrackerManager.Instance.GetStateManager ();
+		
+		// Query the StateManager to retrieve the list of
+		// currently 'active' trackables 
+		//(i.e. the ones currently being tracked by Vuforia)
+		IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours ();
+		
+		// Iterate through the list of active trackables
+		Debug.Log ("List of trackables currently active (tracked): ");
+		int numFrameMarkers = 0;
+		int numImageTargets = 0;
+		int numMultiTargets = 0;
+		foreach (TrackableBehaviour tb in activeTrackables) {
+			Debug.Log("Trackable: " + tb.TrackableName);
+			
+			if (tb is MarkerBehaviour)
+				numFrameMarkers++;
+			else if (tb is ImageTargetBehaviour)
+				numImageTargets++;
+			else if (tb is MultiTargetBehaviour)
+				numMultiTargets++;
+		}
+		
+		Debug.Log ("Found " + numFrameMarkers + " frame markers in curent frame");
+		Debug.Log ("Found " + numImageTargets + " image targets in curent frame");
+		Debug.Log ("Found " + numMultiTargets + " multi-targets in curent frame");
+	}
+}
